1b5cf38300645a4ca0485c3f5a016d9cd2155656
joker4951 <67384841+joker4951@users.noreply.github.com>
2020-07-08 02:07:04 +0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,96 @@
+window.addEventListener('load',()=> {
+    let long;
+    let lat;
+    let wid;
+    let temperatureDescription= document.getElementsByClassName('temperature-description')[0];
+    let temperatureDegree= document.getElementsByClassName('temperature-degree')[0];
+    let locationCity= document.getElementsByClassName('location-city')[0];
+    let locationCountry= document.getElementsByClassName('location-country')[0];
+    let image= document.getElementsByClassName('image')[0];
+
+    if(navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(position => {
+            long = position.coords.longitude;
+            lat = position.coords.latitude;
+            
+            const proxy = 'https://cors-anywhere.herokuapp.com/';
+            const search = `${proxy}https://www.metaweather.com/api/location/search/?lattlong=${lat},${long}`;
+
+            fetch(search)
+                .then(response =>{
+                    return response.json();
+                })
+                .then(data =>{
+                    wid = data[0].woeid;
+                    const proxy = 'https://cors-anywhere.herokuapp.com/';
+                    const weather = `${proxy}https://www.metaweather.com/api/location/${wid}/`;
+
+                    fetch(weather)
+                        .then(response =>{
+                            return response.json();
+                        })
+                        .then(data =>{
+                            console.log(data);
+                            const { applicable_date, humidity, max_temp, min_temp, the_temp, weather_state_name, weather_state_abbr} = data.consolidated_weather[0];
+                            temperatureDegree.textContent= the_temp.toFixed(1);
+                            temperatureDescription.textContent= weather_state_name;
+                            locationCity.textContent = data.title;
+                            locationCountry.textContent = data.parent.title;
+                            const url= `https://www.metaweather.com/static/img/weather/${weather_state_abbr}.svg`;
+                            image.src = url; 
+                        });
+
+                });
+        });
+    }
+});
+
+let input = document.getElementsByClassName('btn')[0];
+
+input.addEventListener('keyup', ()=>{
+    let query;
+    let wid;
+    let temperatureDescription= document.getElementsByClassName('temperature-description')[0];
+    let temperatureDegree= document.getElementsByClassName('temperature-degree')[0];
+    let locationCity= document.getElementsByClassName('location-city')[0];
+    let locationCountry= document.getElementsByClassName('location-country')[0];
+    let image= document.getElementsByClassName('image')[0];
+
+    event.preventDefault();
+    if (event.keyCode === 13) {
+        query=input.value;
+
+        const proxy = 'https://cors-anywhere.herokuapp.com/';
+        const search = `${proxy}https://www.metaweather.com/api/location/search/?query=${query}`;
+
+        fetch(search)
+                .then(response =>{
+                    return response.json();
+                })
+                .then(data =>{
+                    if (data.length<1){
+                        alert("Invalid City Name");
+                    }
+                    else {
+                        wid = data[0].woeid;
+                    const proxy = 'https://cors-anywhere.herokuapp.com/';
+                    const weather = `${proxy}https://www.metaweather.com/api/location/${wid}/`;
+
+                    fetch(weather)
+                        .then(response =>{
+                            return response.json();
+                        })
+                        .then(data =>{
+                            console.log(data);
+                            const { applicable_date, humidity, max_temp, min_temp, the_temp, weather_state_name, weather_state_abbr} = data.consolidated_weather[0];
+                            temperatureDegree.textContent= the_temp.toFixed(1);
+                            temperatureDescription.textContent= weather_state_name;
+                            locationCity.textContent = data.title;
+                            locationCountry.textContent = data.parent.title;
+                            const url= `https://www.metaweather.com/static/img/weather/${weather_state_abbr}.svg`;
+                            image.src = url; 
+                        });
+                    }
+                })
+    }
+});
